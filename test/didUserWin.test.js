8764fbf56248a63ee4e23b9b0efb3be9c99ba3f6
Kayla Schoorl <kaylaschoorl@gmail.com>
2021-03-25 15:04:15 -0700

    Added html and tests

--- test/didUserWin.test.js
+++ test/didUserWin.test.js
@@ -3,14 +3,16 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('results in a draw', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const player = 'rock';
+    const computer = 'rock';
+    const actual = didUserWin(player, computer);
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const expected = 'draw';
 
     //Expect
     // Make assertions about what is expected versus the actual result
