1116ef97daa71ece4aedb40f40840d5d4ef00cf3
Kayla Schoorl <kaylaschoorl@gmail.com>
2021-03-27 20:53:08 -0700

    added spock and lizard

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
 import { didUserWin, randomThrow } from "./utils.js";
 
-// import functions and grab DOM elements
+const playButton = document.querySelector("#play");
+const resultsText = document.querySelector("#results");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const winsText = document.querySelector("#wins");
+const lossesText = document.querySelector("#losses");
+const drawText = document.querySelector("#draws");
+const totalAnswers = document.querySelector("#total");
+const spock = document.querySelector("#spock");
+const lizard = document.querySelector("lizard");
 
-const playButton = document.querySelector("#play")
-const resultsText = document.querySelector("#results")
-const rock = document.querySelector("#rock")
-const paper = document.querySelector("#paper")
-const scissors = document.querySelector("#scissors")
-const winsText = document.querySelector("#wins")
-const lossesText = document.querySelector("#losses")
-const drawText = document.querySelector("#draws")
-const totalAnswers = document.querySelector("#total")
-
-// initialize state
 let wins = 0;
 let losses = 0;
 let draws = 0;
 let total =0;
-// set event listeners to update state and DOM
 
 playButton.addEventListener ('click', () => {
-   
     const player = document.querySelector('input:checked').value;
-
-    
     const computer = randomThrow();
-    
-   
     const results = didUserWin(player, computer);
-    
-    
-    if (results === 'draw') {
+     if (results === 'draw') {
         draws++
         drawText.textContent = `${draws} Draws`
         total++
         totalAnswers.textContent = `${total} total`
-    }
-    
-    
+        };
     if (results === 'loss') {
         losses++
         lossesText.textContent = `${losses} losses`
         total++
         totalAnswers.textContent = `${total} total`
-    }
-
+        };
     if (results === 'win') {
         wins++ 
         winsText.textContent = `${wins} wins`
         total++
         totalAnswers.textContent = `${total} total`
-    }
-
-    
-
+        };
 });
--- utils.js
+++ utils.js
@@ -1,37 +1,66 @@
 export function didUserWin(player, computer) {
     if (player === computer) {
         return 'draw';
-    }
-
-  if (computer === 'rock' && player === 'scissors'){
-    return 'loss'
-}
-
-    if (computer === 'rock' && player === 'paper'){
-    return 'win'
-}
-    
-    if (computer === 'paper' && player === 'scissors'){
-    return 'win'
-}
-
+    };
+    if (computer === 'scissors' && player === 'paper'){
+        return 'loss'
+    };
     if (computer === 'paper' && player === 'rock'){
         return 'loss'
-    }
-
-    if (computer === 'scissors' && player === 'paper'){
-            return 'loss'
-        }
-
+    };
+    if (computer === 'rock' && player === 'lizard'){
+        return 'win'
+    };
+    if (computer ==='lizard' && player === 'spock'){
+        return 'win'
+    };
+    if (computer === 'spock' && player === 'scissors'){
+        return 'win'
+    };
+    if (computer === 'scissors' && player === 'lizard'){
+        return 'win'
+    };
+    if (computer === 'lizard' && player === 'paper'){
+        return 'win'
+    };
+    if (computer === 'paper' && player === 'spock'){
+        return 'win'
+    }; 
+    if (computer === 'spock' && player === 'rock'){
+    return 'win'
+    };
+    if (computer === 'rock' && player === 'scissors'){
+        return 'win'
+    };
+    if (computer === 'lizard' && player === 'rock'){
+        return 'loss'
+    };
+    if (computer ==='spock' && player === 'lizard'){
+        return 'loss'
+    };
+    if (computer === 'scissors' && player === 'spock'){
+        return 'loss'
+    };
+    if (computer === 'lizard' && player === 'scissors'){
+        return 'loss'
+    };
+    if (computer === 'paper' && player === 'lizard'){
+        return 'loss'
+    };
+    if (computer === 'spock' && player === 'paper'){
+        return 'loss'
+    }; 
+    if (computer === 'rock' && player === 'spock'){
+    return 'loss'
+    };
     if (computer === 'scissors' && player === 'rock'){
-                return 'win'
-            }
-            
-    }
+        return 'loss'
+    };
 
-export function randomThrow() {
 
-        const throwNumber = Math.ceil(Math.random() *3);
+    export function randomThrow() {
+
+        const throwNumber = Math.ceil(Math.random() *5);
 
         if (throwNumber === 1) return 'rock';
         
@@ -39,5 +68,9 @@ export function randomThrow() {
 
         if (throwNumber === 3) return 'scissors';
 
-    }
+        if (throwNumber === 4) return 'lizard';
+
+        if (throwNumber === 5) return 'spock';
+
+    };
     
